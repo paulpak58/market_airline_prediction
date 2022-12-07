a01bb26d838142147bf84b42e75375d1dc7b85d5
paulpak58 <paulpak58@gmail.com>
2022-12-07 17:52:26 -0600

    Updated NN

--- nn.py
+++ nn.py
@@ -7,27 +7,32 @@ from data import MarketAirlineDataset
 from torch.utils.data import DataLoader
 
 class NN(nn.Module):
-    def __init__(self,in_features=22,hidden1=256,hidden2=512,hidden3=256,hidden4=128,out_features=1):
+    def __init__(self,in_features=22,hidden=128,hidden1=256,hidden2=512,hidden3=256,hidden4=128,out_features=1):
         super().__init__()
-        self.fc1 = nn.Linear(in_features,hidden1)
-        # self.bn1 = nn.BatchNorm1d(16)
-        self.fc2 = nn.Linear(hidden1,hidden2)
-        self.fc3 = nn.Linear(hidden2,hidden2)
-        self.fc4 = nn.Linear(hidden2,hidden2)
-        self.fc5 = nn.Linear(hidden2,hidden2)
-
-        self.fc6 = nn.Linear(hidden2,hidden3)
-        self.fc7 = nn.Linear(hidden3,hidden4)
-        self.out = nn.Linear(hidden4,out_features)
+        self.fc1 = nn.Linear(in_features,hidden)
+        self.bn1 = nn.BatchNorm1d(hidden)
+        self.fc2 = nn.Linear(hidden,hidden*2)
+        self.bn2 = nn.BatchNorm1d(hidden*2)
+        self.fc3 = nn.Linear(hidden,hidden*4)
+        self.bn3 = nn.BatchNorm1d(hidden*4)
+        self.fc4 = nn.Linear(hidden*4,hidden*8)
+        self.bn4 = nn.BatchNorm1d(hidden*8)
+        self.fc5 = nn.Linear(hidden*8,hidden*8)
+        self.bn5 = nn.BatchNorm1d(hidden*8)
+        self.fc6 = nn.Linear(hidden*8,hidden*4)
+        self.bn6 = nn.BatchNorm1d(hidden*4)
+        self.fc7 = nn.Linear(hidden*4,hidden*2)
+        self.bn7 = nn.BatchNorm1d(hidden*2)
+        self.out = nn.Linear(hidden*2,out_features)
 
     def forward(self,x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-        x = F.relu(self.fc4(x))
-        x = F.relu(self.fc5(x))
-        x = F.relu(self.fc6(x))
-        x = F.relu(self.fc7(x))
+        x = F.relu(self.bn1(self.fc1(x)))
+        x = F.relu(self.bn2(self.fc2(x)))
+        x = F.relu(self.bn3(self.fc3(x)))
+        x = F.relu(self.bn4(self.fc4(x)))
+        x = F.relu(self.bn5(self.fc5(x)))
+        x = F.relu(self.bn6(self.fc6(x)))
+        x = F.relu(self.bn7(self.fc7(x)))
         x = self.out(x)
         return x
 
@@ -137,7 +142,7 @@ if __name__=='__main__':
         )
     else:
         trainer = pl.Trainer(
-            accelerator='gpu',
+            # accelerator='gpu',
             devices=1,
             strategy='ddp',
             num_nodes=1,
