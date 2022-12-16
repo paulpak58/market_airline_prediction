3feebd329e935d6613c6ed47385c01571156206d
paulpak58 <paulpak58@gmail.com>
2022-12-16 14:27:16 -0500

    Counterfactual prediction

---
+++ cf_predict.py
@@ -0,0 +1,27 @@
+import torch
+import pandas as pd
+from collections import OrderedDict
+from nn import NN 
+
+if __name__=='__main__':
+    
+    model = NN(in_features=8,out_features=1)
+    ckpt_dir = 'lightning_logs/largestNN_lagged_epoch400_batch256_lr4e-5/checkpoints/epoch=399-step=206800.ckpt'
+    model_ckpt = torch.load(ckpt_dir)
+    state_dict = OrderedDict()
+    for k,v in model_ckpt['state_dict'].items():
+        if 'model' in k:
+            name = k[6:]
+            state_dict[name] = v
+    msg = model.load_state_dict(state_dict,strict=False)
+    print(msg)
+
+    df = pd.read_csv('data/final_df_with_lag.csv')
+
+    find = df.loc[(df['YEAR']==2019)&(df['QUARTER']==3)&(df['AIRLINE_ID']==7)]
+    x = find[['YEAR','QUARTER','avg_price','tot_passengers','avg_dist','Closed',\
+               'ID','AIRLINE_ID']].values
+    # y = find['avg_price_t1'].values
+    pred = model(torch.tensor(x).float())
+    avg_pred = torch.mean(pred)
+    print('Counterfactual market fare prediction: ', avg_pred.item())
